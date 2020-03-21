3f7faa6b19075d18eeae11c89ae3d4db6306ee9a
wondrousWebWorks <alberts.johann@gmail.com>
2020-03-21 04:08:33 +0000

    Added render_template for index.html

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 if os.path.exists('env.py'):
     import env
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('pages/index.html')
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
