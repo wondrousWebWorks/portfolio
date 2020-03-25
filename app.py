b9367b96991940abef290b2eb5463459d53b5ac5
wondrousWebWorks <alberts.johann@gmail.com>
2020-03-25 16:26:46 +0000

    Imported flask_pymongo and bson.objectid, and instantiated an instance of PyMongo

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import os
 if os.path.exists('env.py'):
     import env
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
-    return render_template('pages/index.html')
+    return render_template('pages/index.html', skills=mongo.db.wondrouswebworks_profile_skills.find())
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=os.environ.get("PORT"),
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=os.environ.get('PORT'),
             debug=True)
