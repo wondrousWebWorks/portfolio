d32fa95a7d3b8fa78332586214a1c03e8577a96e
wondrousWebWorks <alberts.johann@gmail.com>
2020-05-06 22:16:18 +0100

    Moved QUALIFICATIONS- related variables and event listeners here from app.js.  Also modified event listeners for toggling, adding or updating form data.

---
+++ static/js/admin/qualifications.js
@@ -0,0 +1,38 @@
+/*jshint esversion: 6 */
+const qualificationAddModalTrigger = document.querySelector('#add-qualification-btn');
+const qualificationUpdateButtons = document.querySelectorAll('.update-qualification-btn');
+const qualificationDeleteButtons = document.querySelectorAll('.delete-qualification-btn');
+const qualificationModal = document.querySelector('#qualifications-form-modal');
+const qualificationForm = document.querySelector('#qualifications-form');
+const qualificationFormLabels = document.querySelectorAll('.qualifications-form-label');
+const qualificationFormSubmitButton = document.querySelector('#qualifications-form-submit-btn');
+const qualificationFormSubmitButtonText = document.querySelector('#qualifications-form-submit-btn-text');
+const qualificationFormDocId = document.querySelector('#qualification-form-doc-id');
+const qualificationsAlert = document.querySelector('#qualifications-alert');
+const qualificationFormInputs = document.querySelectorAll('#qualifications-form input');
+const qualificationName = document.querySelector('#qualification-name');
+const qualificationFrom = document.querySelector('#qualification-from');
+const qualificationIssueDate = document.querySelector('#qualification-issue-date');
+const qualificationViewUrl = document.querySelector('#qualification-view-url');
+const qualificationInfoUrl = document.querySelector('#qualification-info-url');
+const qualificationModalInstance = M.Modal.init(qualificationModal);
+
+document.addEventListener('DOMContentLoaded', function () {
+    qualificationAddModalTrigger.addEventListener('click', () => {
+        toggleAddModal('qualifications');
+    });
+
+    qualificationFormSubmitButton.addEventListener('click', function() {
+        addOrUpdateFormData(this);
+    });
+
+    Array.from(qualificationUpdateButtons).forEach(updateButton => {
+        updateButton.addEventListener('click', getQualificationData);
+    });
+
+    Array.from(qualificationDeleteButtons).forEach(deleteButton => {
+        deleteButton.addEventListener('click', function() {
+            deleteDocument(this);
+        });
+    });
+});
